69b41fb10151cabfc2887d731629ba4a19843ada
yolkyboy <54444836+yolkyboy@users.noreply.github.com>
2020-10-09 19:42:51 +0700

    String2Int
    7:30 - 7:42

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace String2Int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            //***Getting an input from the user
+            Console.WriteLine("Enter something: ");
+            input = Console.ReadLine();
+            //***Looping every character from the input
+            for (int i = 0; i < input.Length; i++)
+                {
+                    //***Convert each character to ASCII code
+                    int ASCnum = input[i];
+                    //***if the ASCII code is more than or equal to 48, and less than or equal to 57, Do the calculation
+                    if(ASCnum >= 48 && ASCnum <= 57)
+                    {
+                        int Vnum;
+                       //***Get the ASCII code from i character, and minus with 48 to make a result as integer
+                        Vnum = ASCnum - 48;
+                        Console.Write(Vnum);
+                    }
+                }
+        }
+    }
+}
